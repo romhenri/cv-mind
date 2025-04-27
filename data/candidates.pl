d55441d3142a67c6128895c9ab237d2dd1a485c2
Rômulo Henri <romulohenri000@gmail.com>
2025-04-26 23:53:25 -0300

    update: data jobs n candidates

--- data/candidates.pl
+++ data/candidates.pl
@@ -1,3 +1,20 @@
-candidate(101, 'Ana Silva', 8.5, 5, 'ComputerEngineering', 2, ['Python', 'SQL', 'Docker'], ['Back', 'Data']).
-candidate(102, 'Bruno Costa', 7.8, 6, 'Statistics', 3, ['SQL', 'Excel', 'PowerBI'], ['Data']).
-candidate(103, 'Carla Souza', 9.0, 7, 'DataScience', 5, ['Python', 'R', 'MachineLearning'], ['Data']).
+candidate(101, 'Ana Silva', 8.5, 5, 'CC', 2, ['Python', 'SQL', 'Docker'], ['Back', 'Data']).
+candidate(102, 'Bruno Costa', 7.8, 6, 'CDIA', 3, ['SQL', 'Excel', 'PowerBI'], ['Data']).
+candidate(103, 'Carla Souza', 9.0, 7, 'CE', 5, ['Python', 'R', 'ML'], ['Data']).
+candidate(104, 'Diego Martins', 8.2, 4, 'CC', 2, ['Java', 'SpringBoot', 'Docker', 'Kubernetes'], ['Back', 'DevOps']).
+candidate(105, 'Fernanda Lima', 7.6, 5, 'CDIA', 1, ['SQL', 'Python', 'Pandas', 'PowerBI'], ['Data', 'ML']).
+candidate(106, 'Gustavo Torres', 8.9, 3, 'CE', 3, ['C++', 'EmbeddedC', 'RTOS'], ['Embarcados']).
+candidate(107, 'Helena Prado', 9.1, 6, 'CC', 4, ['Python', 'TensorFlow', 'Keras', 'Pandas'], ['ML', 'Data']).
+candidate(108, 'Igor Santos', 8.0, 5, 'CE', 2, ['C', 'C++', 'Microcontrollers'], ['Embarcados']).
+candidate(109, 'Juliana Rocha', 7.7, 2, 'CDIA', 2, ['SQL', 'Excel', 'Tableau'], ['Data']).
+candidate(110, 'Lucas Fernandes', 9.4, 7, 'CC', 5, ['React', 'TypeScript', 'Node.js'], ['Front', 'Back']).
+candidate(111, 'Mariana Ribeiro', 8.6, 6, 'CE', 4, ['Angular', 'JavaScript', 'HTML', 'CSS'], ['Front']).
+candidate(112, 'Nicolas Pereira', 7.5, 4, 'CC', 2, ['Go', 'Docker', 'Kubernetes'], ['Back', 'DevOps']).
+candidate(113, 'Olivia Castro', 9.2, 5, 'CDIA', 3, ['Python', 'SQL', 'MachineLearning'], ['ML', 'Data']).
+candidate(114, 'Pedro Carvalho', 8.1, 6, 'CE', 3, ['Rust', 'C++', 'IoT'], ['Embarcados']).
+candidate(115, 'Quesia Alves', 7.9, 2, 'CC', 1, ['Flutter', 'Dart', 'Firebase'], ['Front']).
+candidate(116, 'Renan Costa', 9.0, 7, 'CDIA', 5, ['SQL', 'Python', 'DeepLearning'], ['ML', 'Data']).
+candidate(117, 'Sara Moura', 8.3, 5, 'CE', 4, ['Java', 'Spring', 'Docker'], ['Back']).
+candidate(118, 'Tiago Goncalves', 8.7, 3, 'CC', 2, ['Node.js', 'Express', 'MongoDB'], ['Back', 'DevOps']).
+candidate(119, 'Ursula Farias', 7.8, 4, 'CDIA', 3, ['HTML', 'CSS', 'JavaScript', 'React'], ['Front']).
+candidate(120, 'Vinicius Lima', 9.5, 8, 'CE', 6, ['Python', 'PyTorch', 'NLP'], ['ML']).
--- data/jobs.pl
+++ data/jobs.pl
@@ -3,18 +3,32 @@
 %%% DATABASE - JOBS %%%
 
 % job(ID, Title, InterestArea, TargetCourse, Min_CRA, Min_Semester, Required, Desired, Min_Exp).
+job(1, 'BackendDeveloper', 'Back', 'CC', 7.5, 4, 
+    ['Python', 'SQL'], ['Docker', 'AWS', 'Git'], 2).
 
-job(1, 'BackendDeveloper', 'Back', 'ComputerEngineering', 7.5, 4, 
-     ['Python', 'SQL'], ['Docker', 'AWS', 'Git'], 2).
+job(2, 'DataAnalyst', 'Data', 'CDIA', 7.0, 5, 
+    ['SQL', 'Excel'], ['Python', 'PowerBI', 'Statistics'], 1).
 
-job(2, 'DataAnalyst', 'Data', 'Statistics', 7.0, 5, 
-     ['SQL', 'Excel'], ['Python', 'PowerBI', 'Statistics'], 1).
+job(3, 'DataScientist', 'Data', 'CDIA', 8.0, 6, 
+    ['Python', 'Statistics'], ['MachineLearning', 'R', 'SQL'], 3).
 
-job(3, 'DataScientist', 'Data', 'DataScience', 8.0, 6, 
-     ['Python', 'Statistics'], ['MachineLearning', 'R', 'SQL'], 3).
+job(4, 'FrontendDeveloper', 'Front', 'CC', 7.5, 4,
+    ['HTML', 'CSS', 'JavaScript'], ['React', 'Vue', 'Figma'], 1).
 
-job(4, 'FrontendDeveloper', 'Front', 'InformationSystems', 7.5, 4,
-     ['HTML', 'CSS', 'JavaScript'], ['React', 'Vue', 'Figma'], 1).
+job(5, 'MachineLearningEngineer', 'ML', 'CDIA', 8.5, 7,
+    ['Python', 'MachineLearning'], ['TensorFlow', 'PyTorch', 'Keras'], 4).
 
-job(5, 'MachineLearningEngineer', 'Data', 'DataScience', 8.5, 7,
-     ['Python', 'MachineLearning'], ['TensorFlow', 'PyTorch', 'Keras'], 4).
+job(6, 'DevOpsEngineer', 'DevOps', 'CE', 8.0, 5,
+    ['Docker', 'Kubernetes'], ['AWS', 'Terraform', 'Git'], 2).
+
+job(7, 'EmbeddedSystemsDeveloper', 'Embarcados', 'CE', 7.5, 4,
+    ['C', 'C++'], ['Microcontrollers', 'RTOS', 'IoT'], 1).
+
+job(8, 'FullstackDeveloper', 'Back', 'CC', 8.2, 5,
+    ['Node.js', 'React'], ['MongoDB', 'Docker', 'Git'], 3).
+
+job(9, 'AIResearcher', 'ML', 'CE', 9.0, 6,
+    ['Python', 'DeepLearning'], ['TensorFlow', 'PyTorch', 'NLP'], 2).
+
+job(10, 'DataEngineer', 'Data', 'CDIA', 7.8, 5,
+    ['SQL', 'Python'], ['ETL', 'BigData', 'AWS'], 3).
