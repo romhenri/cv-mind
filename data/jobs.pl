506a6c1a7a22b9c0e0274aff3683c9af35ca551e
Rômulo Henri <romulohenri000@gmail.com>
2025-04-27 23:53:56 -0300

    fix: wrong data schema

--- data/jobs.pl
+++ data/jobs.pl
@@ -1,34 +1,34 @@
-:- dynamic job/9.
+:- dynamic job/8.
 
 %%% DATABASE - JOBS %%%
 
-% job(ID, Title, InterestArea, TargetCourse, Min_CRA, Min_Semester, Required, Desired, Min_Exp).
+% job(ID, Title, InterestArea, TargetCourse, Min_CRA, Min_Semester, DesiredSkills, Min_Exp).
 job(1, 'BackendDeveloper', 'Back', 'CC', 7.5, 4, 
-    ['Python', 'SQL'], ['Docker', 'AWS', 'Git'], 2).
+    ['Python', 'SQL', 'Docker', 'AWS', 'Git'], 2).
 
 job(2, 'DataAnalyst', 'Data', 'CDIA', 7.0, 5, 
-    ['SQL', 'Excel'], ['Python', 'PowerBI', 'Statistics'], 1).
+    ['SQL', 'Excel', 'Python', 'PowerBI', 'Statistics'], 1).
 
 job(3, 'DataScientist', 'Data', 'CDIA', 8.0, 6, 
-    ['Python', 'Statistics'], ['MachineLearning', 'R', 'SQL'], 3).
+    ['Python', 'Statistics', 'ML', 'R', 'SQL'], 3).
 
 job(4, 'FrontendDeveloper', 'Front', 'CC', 7.5, 4,
-    ['HTML', 'CSS', 'JavaScript'], ['React', 'Vue', 'Figma'], 1).
+    ['HTML', 'CSS', 'JavaScript', 'React', 'Vue', 'Figma'], 1).
 
 job(5, 'MachineLearningEngineer', 'ML', 'CDIA', 8.5, 7,
-    ['Python', 'MachineLearning'], ['TensorFlow', 'PyTorch', 'Keras'], 4).
+    ['Python', 'ML', 'TensorFlow', 'PyTorch', 'Keras'], 4).
 
 job(6, 'DevOpsEngineer', 'DevOps', 'CE', 8.0, 5,
-    ['Docker', 'Kubernetes'], ['AWS', 'Terraform', 'Git'], 2).
+    ['Docker', 'Kubernetes', 'AWS', 'Terraform', 'Git'], 2).
 
 job(7, 'EmbeddedSystemsDeveloper', 'Embarcados', 'CE', 7.5, 4,
-    ['C', 'C++'], ['Microcontrollers', 'RTOS', 'IoT'], 1).
+    ['C', 'C++', 'Microcontrollers', 'RTOS', 'IoT'], 1).
 
 job(8, 'FullstackDeveloper', 'Back', 'CC', 8.2, 5,
-    ['Node.js', 'React'], ['MongoDB', 'Docker', 'Git'], 3).
+    ['Node.js', 'React', 'MongoDB', 'Docker', 'Git'], 3).
 
 job(9, 'AIResearcher', 'ML', 'CE', 9.0, 6,
-    ['Python', 'DeepLearning'], ['TensorFlow', 'PyTorch', 'NLP'], 2).
+    ['Python', 'DeepLearning', 'TensorFlow', 'PyTorch', 'NLP'], 2).
 
 job(10, 'DataEngineer', 'Data', 'CDIA', 7.8, 5,
-    ['SQL', 'Python'], ['ETL', 'BigData', 'AWS'], 3).
+    ['SQL', 'Python', 'ETL', 'BigData', 'AWS'], 3).
---
+++ import/out.pl
@@ -0,0 +1,7 @@
+candidate(101, 'Ana Silva', 8.5, 5, 'ComputerEngineering', 2, ['Python','SQL','Docker'], ['Back','Data']).
+candidate(102, 'Bruno Costa', 7.8, 6, 'Statistics', 3, ['SQL','Excel','PowerBI'], ['Data']).
+candidate(103, 'Carla Souza', 9.0, 7, 'DataScience', 5, ['Python','R','MachineLearning'], ['Data']).
+candidate(104, 'Daniel Rocha', 8.2, 4, 'InformationSystems', 1, ['Java','SpringBoot'], ['Back']).
+candidate(105, 'Fernanda Lima', 7.5, 5, 'Mathematics', 2, ['SQL','R','Excel'], ['Data']).
+candidate(106, 'Gabriel Mendes', 9.3, 6, 'ComputerScience', 4, ['Python','C++','AIResearch'], ['Back','Research']).
+candidate(107, 'Larissa Pinto', 8.7, 3, 'SoftwareEngineering', 2, ['Python','Flutter','Git'], ['Mobile','Front']).
